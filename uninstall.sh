7ced8816285ee5ca818c9ab5d5f50d43ef1df3ce
Paul Mendoza <falconwizard01@gmail.com>
2025-07-18 12:57:33 +0800

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# FalconHack Uninstaller
+echo -e "\033[1;31m[!] Removing FalconHack...\033[0m"
+rm -rf Tools/ zphisher/ CamPhish/ subscan/ fast-mail-bomber/ DDoS-Ripper/ track-ip/ dorks-eye/ hackerpro/ RED_HAWK/ TigerVirus/ info-site/ BadMod/ facebash/ DARKARMY/ Auto_Tor_IP_changer/
+echo -e "\033[1;32m[+] All tools removed!\033[0m"
